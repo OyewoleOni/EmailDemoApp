d1f4d83330c57d0f830f8f784bef947dac079f82
Oyewole Oni <ooni@eminenttechnology.com>
2021-03-01 14:30:57 +0100

    Fluent email with RazorRenderer

--- EmailDemo/Program.cs
+++ EmailDemo/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using FluentEmail.Core;
+using FluentEmail.Razor;
+using FluentEmail.Smtp;
+using System;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EmailDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sender = new SmtpSender(() => new SmtpClient("localhost")
+            {
+                EnableSsl = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Port = 25
+               // DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory,
+               // PickupDirectoryLocation = @"C:\oni\Demos"
+            });
+
+            StringBuilder template = new();
+            template.AppendLine("Dear @Model.FirstName,");
+            template.AppendLine("<p>Thnaks for purchasing @Model.ProductName. We hope you enjoyed it. </p>");
+            template.AppendLine("- The OniCo Team");
+
+            Email.DefaultSender = sender;
+            Email.DefaultRenderer = new RazorRenderer();
+            var email = await Email
+                    .From("oni@onico.com")
+                    .To("test@test.com", "ayo")
+                    .Subject("Thanks")
+                    .UsingTemplate(template.ToString(), new { FirstName = "Oni", ProductName = "Boli-wrapped Boli"})
+                    //.Body("Thanks for buying our product")
+                    .SendAsync();
         }
     }
 }
